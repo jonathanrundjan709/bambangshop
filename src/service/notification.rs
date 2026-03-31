f75715c600ecf9e7a4c98615d00de36877e7b0dc
jonathanrundjan <jonathan.yitskhaq@ui.ac.id>
2026-03-31 16:36:22 +0700

    Implement publish function in Program service and Program controller.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -31,7 +31,7 @@ impl NotificationService {
         return Ok(result.unwrap());
     }
 
-    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
+    pub fn notify(product_type: &str, status: &str, product: Product) {
         let mut payload: Notification = Notification {
             product_title: product.clone().title,
             product_type: String::from(product_type),
